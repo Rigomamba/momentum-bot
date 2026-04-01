949f05e261e0cf4e058cf95ef84760284b1b4a6a
Rigomamba <rluquin41@yahoo.com>
2026-03-31 20:17:47 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,11 @@
 import requests
 import os
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import Updater, CommandHandler
 
 TOKEN = os.getenv("BOT_TOKEN")
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("🚀 Momentum Sniper is LIVE\nUse /hot")
+def start(update, context):
+    update.message.reply_text("🚀 Momentum Sniper is LIVE\nUse /hot")
 
 def get_trending():
     url = "https://api.dexscreener.com/latest/dex/search?q=sol"
@@ -26,13 +25,16 @@ def get_trending():
 
     return "\n---\n".join(tokens)
 
-async def hot(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("🔥 Scanning...")
+def hot(update, context):
+    update.message.reply_text("🔥 Scanning...")
     data = get_trending()
-    await update.message.reply_text(data)
+    update.message.reply_text(data)
 
-app = ApplicationBuilder().token(TOKEN).build()
-app.add_handler(CommandHandler("start", start))
-app.add_handler(CommandHandler("hot", hot))
+updater = Updater(TOKEN, use_context=True)
 
-app.run_polling()
+dp = updater.dispatcher
+dp.add_handler(CommandHandler("start", start))
+dp.add_handler(CommandHandler("hot", hot))
+
+updater.start_polling()
+updater.idle()
